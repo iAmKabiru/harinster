2bc473f1f6a536bb6e3478264d4442a3197f85f8
Kabiru Abdulhamid <abdulhamidkabiru@gmail.com>
2019-08-02 19:59:55 +0100

    fix media files

--- harinster/settings.py
+++ harinster/settings.py
@@ -27,8 +27,8 @@ SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 
-#DEBUG = True
-DEBUG = config('DEBUG', default=False, cast=bool) 
+DEBUG = True
+#DEBUG = config('DEBUG', default=False, cast=bool) 
 
 ALLOWED_HOSTS = ['*']
 
