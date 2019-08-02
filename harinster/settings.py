0b0d98ee74c6bbd052a2635aa0418ac57ba42651
Kabiru Abdulhamid <abdulhamidkabiru@gmail.com>
2019-08-02 19:56:44 +0100

    fix media files

--- harinster/settings.py
+++ harinster/settings.py
@@ -165,7 +165,7 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, "live-static", "media-root")
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media').replace('\\', '/')
 
 #---------------------------------------------------------
 
