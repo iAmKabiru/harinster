e9c45c7ed923bd62fa1ca1bc1c530e184ec0f33a
Kabiru Abdulhamid <abdulhamidkabiru@gmail.com>
2019-12-08 14:59:41 -0800

    recreate db

--- harinster/settings.py
+++ harinster/settings.py
@@ -11,8 +11,8 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
-from decouple import config
-import dj_database_url
+# from decouple import config
+# import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -22,8 +22,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-#SECRET_KEY = 'v7657tr#feman4_3o2(!ruqty3dcmx*bn23-a^klvm1e!0yo!3'
-SECRET_KEY = config('SECRET_KEY') 
+SECRET_KEY = 'v7657tr#feman4_3o2(!ruqty3dcmx*bn23-a^klvm1e!0yo!3'
+# SECRET_KEY = config('SECRET_KEY') 
 
 # SECURITY WARNING: don't run with debug turned on in production!
 
@@ -87,21 +87,16 @@ WSGI_APPLICATION = 'harinster.wsgi.application'
 
 
 # HEROKU DATABASE CONFIG
-DATABASES = { 'default':
-dj_database_url.config( default=config('DATABASE_URL') ) }
+# DATABASES = { 'default':
+# dj_database_url.config( default=config('DATABASE_URL') ) }
 
-#DATABASES = {
- #   'default': {
-        #'ENGINE': 'django.db.backends.sqlite3',
-        #'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-  #      'ENGINE': 'django.db.backends.postgresql_psycopg2',
-   #     'NAME':  'harinster',
-    #    'USER' : 'harinster',
-     #   'PASSWORD' : 'harinster',
-      #  'HOST' : 'localhost',
-       # 'PORT' : '',
-    #}
-#}
+DATABASES = {
+   'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+       
+    }
+}
 
 
 # Password validation
